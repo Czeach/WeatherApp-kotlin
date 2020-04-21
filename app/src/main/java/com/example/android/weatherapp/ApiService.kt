1968c0427d07422eb7203c6625be070915e51314
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2020-04-21 18:49:27 +0100

    api service class

--- app/src/main/java/com/example/android/weatherapp/ApiService.kt
+++ app/src/main/java/com/example/android/weatherapp/ApiService.kt
@@ -1,2 +1,30 @@
 package com.example.android.weatherapp
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+//private val retrofit = Retrofit.Builder()
+//    .baseUrl("https://api.openweathermap.org/data/2.5/")
+//    .addConverterFactory(GsonConverterFactory.create())
+//    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+//    .build()
+
+// A retrofit service to fetch a weather info.
+interface OpenWeatherMapApi {
+    @GET("weather?q=Enugu,Enugu&appid=c71d7cc39b34e871d2c9525737fdd3b8")
+    suspend fun getWeather(): String
+}
+
+// this is the main entry point for the network access. Call like `Network.weather.getWeather()`
+object Network {
+    // Configure retrofit to parse JSON and use coroutines
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.openweathermap.org/data/2.5/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    val weather = retrofit.create(OpenWeatherMapApi::class.java)
+}
